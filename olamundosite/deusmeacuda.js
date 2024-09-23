5745f745f221d441c24dc0c8bbd271c473a548a1
Patrícia Seren <webserennn@gmail.com>
2024-09-22 22:28:01 -0300

    vai

--- olamundosite/deusmeacuda.js
+++
@@ -1,8 +0,0 @@
-let index = 0;
-const slides = document.querySelectorAll('.slider img');
-
-setInterval(() => {
-    slides[index].style.display = 'none';
-    index = (index + 1) % slides.length;
-    slides[index].style.display = 'block';
-}, 3000); // Troca de imagem a cada 3 segundos
